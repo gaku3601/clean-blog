6adb88be94298770d90e50c2d52025cfa261cb99
日高岳大 <gaku@mac.local>
2019-02-04 18:17:31 +0900

    signin処理を追加

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -13,6 +13,7 @@ func init() {
 	userController := interfaces.NewUserController(NewSqlHandler())
 
 	router.POST("/users", func(c *gin.Context) { userController.Create(c) })
+	router.POST("/signin", func(c *gin.Context) { userController.SignIn(c) })
 
 	Router = router
 }
